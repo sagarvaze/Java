477f48d6406c66940f6e64fe09132d6e99660d4b
Sagar Vaze <vaze.sagar@gmail.com>
2020-03-14 15:54:36 -0700

    Factorial program

---
+++ NumericFunctions.java
@@ -0,0 +1,17 @@
+public class NumericFunctions {
+    public static int factorial(int n) {
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+    return result;
+    }
+
+    public static void main(String[] args) {
+        for (int n = 1; n <= 10; n++) {
+            System.out.print(n);
+            System.out.print(" ");
+            System.out.println(factorial(n));
+        }
+    }
+}
